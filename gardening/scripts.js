34acc4bcfc958e8027946d3e4e8bc9ceb498de15
svnalx <alex@alex-crist.com>
2021-09-03 01:19:54 -0500

    almost done, needs styling

--- gardening/scripts.js
+++ gardening/scripts.js
@@ -4,24 +4,28 @@ const ele_response = document.querySelectorAll('#response')[0];
 const ele_form_error = document.querySelectorAll('#form_error')[0];
 const zipRegex = /^\d{5}$/;
 
-
-
 ele_submit.onclick = function() {
   let zip = ele_zip.value;
-  if (zipRegex.test(zip)) {
 
+  if (zipRegex.test(zip)) {
     document.querySelectorAll('#form_error')[0].classList.add('d-none');
+    document.querySelector('#map').remove();
+    let ele_map = document.createElement('div');
+    ele_map.setAttribute('id', 'map');
+    document.querySelector('#response').append(ele_map);
 
     axios.get(`https://phzmapi.org/${zip}.json`)
     .then(function (response) {
       console.log(response.data);
       document.querySelector('#response').classList.remove('d-none');
-      document.querySelectorAll('#response p span')[0].innerText = zip;
-      document.querySelectorAll('#response h2')[0].innerText = response.data.zone;
 
       /* Make the map */
+      let lat = response.data.coordinates.lat;
+      let lng = response.data.coordinates.lon;
+      let latlng = L.latLng(lat, lng);
+
       let map = L.map('map', {
-        center: [51.505, -0.09],
+        center: [lat, lng],
         zoom: 13
       });
       L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token=pk.eyJ1Ijoic3ZuYWx4IiwiYSI6ImNrdDN4eGhmMzBiZXoydnE4bHZjbjNpaXkifQ.fGag40eXeaOEFgf5h8RXSQ', {
@@ -32,6 +36,10 @@ ele_submit.onclick = function() {
           zoomOffset: -1,
           accessToken: 'pk.eyJ1Ijoic3ZuYWx4IiwiYSI6ImNrdDN4eGhmMzBiZXoydnE4bHZjbjNpaXkifQ.fGag40eXeaOEFgf5h8RXSQ'
       }).addTo(map);
+      var popup = L.popup()
+      .setLatLng(latlng)
+      .setContent('<h4>Zone <strong>'+response.data.zone+'</strong></h4>')
+      .openOn(map);
 
     })
     .catch(function (error) {
